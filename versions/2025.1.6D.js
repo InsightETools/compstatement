342e469ec3d47ee7f4a4403e00d17820bb2783a3
InsightETools <corbin.harder@payplusbenefits.com>
2025-10-23 13:02:38 -0700

    Update 2025.1.6D.js

--- versions/2025.1.6D.js
+++ versions/2025.1.6D.js
@@ -1236,31 +1236,24 @@ async function renderAll(data) {
     console.log(data);
     document.querySelectorAll('[px="headerEmployeeNameSize"]').forEach((el) => {
       el.style.fontSize = data.headerEmployeeNameSize + "px";
-      console.log(data.headerEmployeeNameSize);
     });
     document.querySelectorAll('[px="bodyFontSize"]').forEach((el) => {
       el.style.fontSize = data.bodyFontSize + "px";
-      console.log(data.bodyFontSize);
     });
     document.querySelectorAll('[px="headerYearSize"]').forEach((el) => {
       el.style.fontSize = data.headerYearSize + "px";
-      console.log(data.headerYearSize);
     });
     document.querySelectorAll('[px="headerNameSize"]').forEach((el) => {
       el.style.fontSize = data.headerNameSize + "px";
-      console.log(data.headerNameSize);
     });
     document.querySelectorAll('[px="headerEmployeeSize"]').forEach((el) => {
       el.style.fontSize = data.headerEmployeeSize + "px";
-      console.log(data.headerEmployeeSize);
     });
     document.querySelectorAll('[color="primaryColor"]').forEach((el) => {
       el.style.color = elementColor.primaryColor;
-      console.log(elementColor.primaryColor);
     });
     document.querySelectorAll('[color="secondaryColor"]').forEach((el) => {
       el.style.color = elementColor.secondaryColor;
-      console.log(elementColor.secondaryColor);
     });
   }
 
